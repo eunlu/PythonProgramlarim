59738570c2541e627e226995f18791756134b47e
eunlu <erkan9unlu@gmail.com>
2018-01-18 11:48:57 +0300

    Faktoriyel uygulaması

---
+++ faktoriyelHesaplama.py
@@ -0,0 +1,28 @@
+# -- coding: cp1254 --
+
+#--------------------------------------------------------------
+# @Yazar     : Erkan Ünlü
+# @Proje Adý : Programlar
+# @Dosya Adý : faktoriyelHesaplama.py
+# @Zaman     : 26.12.2017 17:37
+# @Lisans    : GNU/GPLv3
+# -------------------------------------------------------------
+# Normal fonksiyonla hesaplama
+def fakt1(n):
+    fakt = 1
+
+    for x in range(1, n + 1):
+        fakt *= x
+    print n, "!=1x2x3...", n, "=", fakt
+    print "x=", x
+# recursive fonksiyon olarak hesaplayalým
+def fakt2(n):
+    if n==1:
+        return 1
+    else:
+        return n*fakt2(n-1)
+
+
+sayi = input("Faktoriyelini hesaplatmak istediðiniz sayý: ")
+print fakt1(sayi)
+print fakt2(sayi)
